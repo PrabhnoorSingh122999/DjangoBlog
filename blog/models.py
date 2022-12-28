61d6d0cd2773b748978f2220ce7bfd828eff2f32
Prabhnoor Singh <72328658+PrabhnoorSingh122999@users.noreply.github.com>
2022-12-28 22:21:41 +0530

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -11,3 +11,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    def __str__(self):
+        return self.title
