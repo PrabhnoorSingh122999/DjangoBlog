90982f76efafc34d9ebfaa1758532fd6895f0e8b
Prabhnoor Singh <72328658+PrabhnoorSingh122999@users.noreply.github.com>
2022-12-28 22:13:46 +0530

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -3,3 +3,9 @@ from .models import Post
 # Register your models here.
 
 admin.site.register(Post)
+
+admin.site.register(Health)
+admin.site.register(Environment)
+admin.site.register(Politics)
+admin.site.register(Sports)
+admin.site.register(Travel)
