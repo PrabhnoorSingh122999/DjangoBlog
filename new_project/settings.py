f394a4f1d530cf36393e9b6e4622a1647bc360f1
Prabhnoor Singh <72328658+PrabhnoorSingh122999@users.noreply.github.com>
2022-12-30 17:57:22 +0530

    Update settings.py

--- new_project/settings.py
+++ new_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'categories.apps.CategoriesConfig',
     'blog.apps.BlogConfig', 
     'users.apps.UsersConfig',
     'crispy_forms', 
