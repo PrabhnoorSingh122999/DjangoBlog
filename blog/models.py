b03711164ec627363a82f9cbfab549920e280486
hamishthakur <hamishthakur@gmail.com>
2023-01-04 22:49:43 +0530

    Category

--- blog/models.py
+++ blog/models.py
@@ -1,19 +1,21 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from categories.models import Category
 
 
-class Category(models.Model):
-    title = models.CharField(max_length=100)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    def __str__(self):
-        return self.title
+# class Category(models.Model):
+#     title = models.CharField(max_length=100)
+#     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+#     def __str__(self):
+#         return self.title
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
